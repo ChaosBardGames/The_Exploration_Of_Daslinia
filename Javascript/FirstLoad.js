2419fe8a68134e646749b702fab138b07e12a466
A. Guy <GarathX2@outlook.com>
2026-05-25 14:50:59 +0000

    Create FirstLoad.js

---
+++ Javascript/FirstLoad.js
@@ -0,0 +1,27 @@
+function First_Load {
+  var PageToChange = 1;
+  const Pages = [
+    "Title Card",
+    "Intro_I",
+    "Intro_II",
+    "Intro_III",
+    "Intro_IV",
+    "Intro_V",
+    "Intro_VI",
+  ];
+    for(let PagesLength of Pages) {
+      
+      let Current_Page = 1;
+      
+      if (!(PagesLength === PageToChange)) {
+        const PageToHide = Pages[Current_Page];
+        PageToHide.style.display = "none";
+      } else {
+        const PageToShow = Pages[Current_Page];
+        PageToShow.style.display = "block";
+      }
+      
+      Current_Page = Current_Page++; 
+      
+    }
+}

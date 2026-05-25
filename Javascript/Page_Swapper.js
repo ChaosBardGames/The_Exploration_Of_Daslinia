46316a47942490fa8b350529c7400f60f66d08b9
A. Guy <GarathX2@outlook.com>
2026-05-25 17:02:36 +0000

    Fix Page_Swapper function syntax and logic

--- Javascript/Page_Swapper.js
+++ Javascript/Page_Swapper.js
@@ -1,2 +1,13 @@
 function Page_Swapper {
+  
+  for(let i = 0; i < Pages.length; i++) {  
+    
+    const Page_Name = Pages[i];
+    
+    if (i !== Page_To_Change) {
+      document.getElementById(Page_Name).style.display = "none";
+    } else {
+      document.getElementById(Page_Name).style.display = "block";
+    }
+  }
 }
